e65e8956903d95255cad65183768504b24139073
IDM-VTON <IDM-VTON@gmail.com>
2024-03-20 15:26:50 +0900

    add paper link

--- src/PDFBtn.tsx
+++ src/PDFBtn.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const PDFBtn = ({ placeholder }: Props) => {
   const handleClickArxiv: MouseEventHandler<HTMLButtonElement> = () => {
-    window.open("/ECCV_PaperId_11626.pdf");
+    window.open("https://arxiv.org/abs/2403.05139");
   };
 
   return (
