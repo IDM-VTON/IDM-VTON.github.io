c7cf6641fc042bef2cfd77ec4be7251cf39eba5d
IDM-VTON <IDM-VTON@gmail.com>
2024-03-29 03:34:22 +0900

    update demo

--- src/DEMOBtn.tsx
+++ src/DEMOBtn.tsx
@@ -6,15 +6,15 @@ interface Props {
 }
 
 const DEMOBtn = ({ placeholder }: Props) => {
-  const handleClickGithub: MouseEventHandler<HTMLButtonElement> = () => {
+  const handleClickHuggingface: MouseEventHandler<HTMLButtonElement> = () => {
     // window.open("", "_blank");
     window.open("https://huggingface.co/spaces/yisol/IDM-VTON");
   };
 
   return (
     <button
-      className="rounded-3xl flex items-center px-3 py-1 border border-solid border-gray-500 bg-gray-500 text-white mt-4 cursor-pointer ml-3"
-      onClick={handleClickGithub}
+      className="rounded-3xl flex items-center px-3 py-1 border border-solid border-gray-500 bg-gray-500 text-white mt-4 cursor-pointer mx-auto"
+      onClick={handleClickHuggingface}
     >
       <DEMOSvg />
       <div>{placeholder}</div>
